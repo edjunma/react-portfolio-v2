6d82d2498aa3e843191acc99188b12530aa8ed95
edjunma <junwenma@buffalo.edu>
2019-07-02 00:57:41 -0400

    adding landing page css

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,17 @@ class Landing extends Component {
 			<div style={{ width: '100%', margin: 'auto' }}>
 				<Grid className='landing-grid'>
 					<Cell col={12}>
-						<img src=""></img>
+						<img
+							src='https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png'
+							alt='avatar'
+							className='avatar-img'
+						/>
+
+						<div className='banner-text'>
+							<h1>Full Stack Web Developer</h1>
+							<hr />
+							<p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
+						</div>
 					</Cell>
 				</Grid>
 			</div>
