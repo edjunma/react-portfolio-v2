02b226b6bd09f99a736350039a636cb5d2c7c3dc
edjunma <junwenma@buffalo.edu>
2019-07-02 02:27:42 -0400

    working on project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { activeTab: 0 };
+	}
+
+	toggleCategories() {
+		if (this.state.activeTab === 0) {
+			return (
+				<div>
+					<h1>This is React</h1>
+				</div>
+			);
+		}
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>Project Page</h1>
+			<div className='category-tabs'>
+				<Tabs
+					activeTab={this.state.activeTab}
+					onChange={tabId => this.setState({ activeTab: tabId })}
+					ripple
+				>
+					<Tab>React</Tab>
+					<Tab>Express</Tab>
+					<Tab>NodeJS</Tab>
+					<Tab>MongoDB</Tab>
+				</Tabs>
+
+				<section className='projects-grid'>{this.toggleCategories()}</section>
 			</div>
 		);
 	}
